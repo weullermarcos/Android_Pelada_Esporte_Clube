160e108b75ed69227abafe8aa1428cd1e6ea49ad
Weuller Marcos <weullermarcos@MBP-de-Weuller.lan>
2017-09-10 11:12:15 -0300

    requisitando permissao para localizacao e criando classe de usuario

--- PeladaEsporteClube/app/src/main/java/com/example/weuller/peladaesporteclube/MainActivity.java
+++ PeladaEsporteClube/app/src/main/java/com/example/weuller/peladaesporteclube/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.weuller.peladaesporteclube;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,20 +17,37 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_FINE_LOCATION = 0;
+
     private FirebaseAuth mAuth;
 
     private Button btnExit, btnMap, btnChat;
     private TextView txtUser;
+
+    AlertDialog alert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        loadPermissions(android.Manifest.permission.ACCESS_FINE_LOCATION, REQUEST_FINE_LOCATION);
+
         btnExit = (Button) findViewById(R.id.main_btnExit);
         btnMap = (Button) findViewById(R.id.main_btnMap);
         btnChat = (Button) findViewById(R.id.main_btnChat);
         txtUser = (TextView) findViewById(R.id.main_txtUser);
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            //NÀO TEM PERMISSÃO para localização DESABILITA O BOTÃO
+            btnMap.setEnabled(false);
+        }
+        else{
+
+            //TEM PERMISSÃO para localização hABILITA O BOTÃO
+            btnMap.setEnabled(true);
+        }
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -71,5 +94,49 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        switch (requestCode) {
+
+            case REQUEST_FINE_LOCATION: {
+
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    btnMap.setEnabled(true);
+                }
+                else{
+
+                    btnMap.setEnabled(false);
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("Aviso");
+                    builder.setMessage("Algumas funcionalidades podem não funcionar corretamente, caso a permissão de localização não seja concedida!");
+                    builder.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    });
+
+                    alert = builder.create();
+                    alert.show();
+                }
+                return;
+            }
+        }
+    }
+
+
+    private void loadPermissions(String perm, int requestCode) {
+
+        if (ContextCompat.checkSelfPermission(this, perm) != PackageManager.PERMISSION_GRANTED) {
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(this, perm)) {
+                ActivityCompat.requestPermissions(this, new String[]{perm},requestCode);
+            }
+        }
     }
 }
---
+++ PeladaEsporteClube/app/src/main/java/com/example/weuller/peladaesporteclube/Models/User.java
@@ -0,0 +1,56 @@
+package com.example.weuller.peladaesporteclube.Models;
+
+/**
+ * Created by ismael on 10/09/17.
+ */
+
+public class User {
+
+
+    String email;
+    String name;
+    String uid;
+    Double latitude;
+    Double longitude;
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+}
