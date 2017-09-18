748adf3fadc43c0cf81f9b758817db9e76c059db
Weuller Marcos da Silva Santos <weuller@redecfa.org.br>
2017-09-18 11:21:29 -0300

    criando pickers de data e hora

--- PeladaEsporteClube/app/src/main/java/com/example/weuller/peladaesporteclube/ScheduleFootballActivity.java
+++ PeladaEsporteClube/app/src/main/java/com/example/weuller/peladaesporteclube/ScheduleFootballActivity.java
@@ -2,12 +2,57 @@ package com.example.weuller.peladaesporteclube;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ScheduleFootballActivity extends AppCompatActivity {
+
+    private Button btnTimePicker, btnDatePicker;
+    private String selectedDate, selectedTime;
+
+    public String getSelectedDate() {
+        return selectedDate;
+    }
+
+    public void setSelectedDate(String selectedDate) {
+        this.selectedDate = selectedDate;
+    }
+
+    public String getSelectedTime() {
+        return selectedTime;
+    }
+
+    public void setSelectedTime(String selectedTime) {
+        this.selectedTime = selectedTime;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_football);
+
+        btnTimePicker = (Button) findViewById(R.id.btnTimePicker);
+        btnDatePicker = (Button) findViewById(R.id.btnDatePicker);
+
+
+        btnTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TimePickerFragment fragment = new TimePickerFragment();
+                fragment.show(getSupportFragmentManager(), "timePicker");
+
+            }
+        });
+
+        btnDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DatePickerFragment fragment = new DatePickerFragment();
+                fragment.show(getSupportFragmentManager(), "datePicker");
+
+            }
+        });
     }
 }
---
+++ PeladaEsporteClube/app/src/main/java/com/example/weuller/peladaesporteclube/TimePickerFragment.java
@@ -0,0 +1,48 @@
+package com.example.weuller.peladaesporteclube;
+
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import java.util.Calendar;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.text.format.DateFormat;
+import android.widget.TimePicker;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener{
+
+
+    public TimePickerFragment() {
+        // Required empty public constructor
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        // Use the current time as the default values for the picker
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        // Create a new instance of TimePickerDialog and return it
+        return new TimePickerDialog(getActivity(), this, hour, minute, DateFormat.is24HourFormat(getActivity()));
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+        ScheduleFootballActivity scheduleFootballActivity = (ScheduleFootballActivity) getActivity();
+
+        String selectedTime = Integer.toString(hourOfDay) + ":" + Integer.toString(minute);
+
+        scheduleFootballActivity.setSelectedTime(selectedTime);
+
+    }
+}
