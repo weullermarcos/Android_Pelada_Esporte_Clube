aaa723abc07450fb49e1f25f869f91e45332624e
Weuller Marcos da Silva Santos <weuller@redecfa.org.br>
2017-09-18 17:47:31 -0300

    listando quadras sugeridas, na tela de marcar futebol

--- PeladaEsporteClube/app/src/main/java/com/example/weuller/peladaesporteclube/ScheduleFootballActivity.java
+++ PeladaEsporteClube/app/src/main/java/com/example/weuller/peladaesporteclube/ScheduleFootballActivity.java
@@ -2,6 +2,7 @@ package com.example.weuller.peladaesporteclube;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -9,8 +10,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.example.weuller.peladaesporteclube.Models.FootballField;
+import com.example.weuller.peladaesporteclube.Services.DialogService;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+import java.util.Random;
 
 public class ScheduleFootballActivity extends AppCompatActivity {
 
@@ -21,6 +39,16 @@ public class ScheduleFootballActivity extends AppCompatActivity {
     private ListView lstSuggested;
 
     private ArrayAdapter<String> adpType;
+    private ArrayAdapter<String> adpSuggested;
+
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+
+    private List<FootballField> footballFields = new ArrayList<>();
+
+    private DialogService dialog = new DialogService();
+
 
     public String getSelectedDate() {
         return selectedDate;
@@ -59,6 +87,10 @@ public class ScheduleFootballActivity extends AppCompatActivity {
         adpType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spnType.setAdapter(adpType);
 
+        //configurando adapter de quadras sugeridas
+        adpSuggested = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        lstSuggested.setAdapter(adpSuggested);
+
         populaAdapter();
 
         edtDate.setOnTouchListener(new View.OnTouchListener() {
@@ -91,7 +123,9 @@ public class ScheduleFootballActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-
+                adpSuggested.clear();
+                dialog.showProgressDialog("Carregando informações...", "Aguarde", ScheduleFootballActivity.this);
+                loadFootballFields();
             }
         });
     }
@@ -120,5 +154,69 @@ public class ScheduleFootballActivity extends AppCompatActivity {
         adpType.add("Campo");
         adpType.add("Quadra");
         adpType.add("Society");
+    }
+
+    public void loadFootballFields() {
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("footballField");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                try {
+
+                    footballFields.clear();
+
+                    for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+
+                        FootballField footballField = postSnapshot.getValue(FootballField.class);
+                        footballFields.add(footballField);
+                    }
+
+                    //TODO: requisitar do servidor as quadras sugeridas, atualmente está sendo feito de forma aleatoria
+
+                    Random gerador = new Random();
+                    int num1 = gerador.nextInt(footballFields.size() - 1);
+                    int num2 = gerador.nextInt(footballFields.size() - 1);
+                    int count = 0;
+
+                    for (FootballField footballField : footballFields) {
+
+                        if(count == num1 || count ==num2){
+
+                            footballField.setSugested(true);
+
+                            //adiciona quadra sugerida
+                            adpSuggested.add(footballField.getName());
+                        }
+                        else {
+
+                            footballField.setSugested(false);
+                        }
+
+                        count ++;
+                    }
+
+                    //TODO: atualizar no FIREBASE lista de quadas sugeridas
+
+
+                } catch (Exception e) {
+                    Toast.makeText(ScheduleFootballActivity.this, "Erro ao carregar informações de quadras.", Toast.LENGTH_SHORT).show();
+                } finally {
+                    dialog.hideProgressDialog();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+
+                Log.w("LOG", "Failed to read value.", error.toException());
+
+                dialog.hideProgressDialog();
+                Toast.makeText(ScheduleFootballActivity.this, "Erro ao carregar informações de quadras. verifique a sua conexão com a internet.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
