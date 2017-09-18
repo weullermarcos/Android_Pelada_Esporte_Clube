8370281354491618cb1dce2b8254d103da59f8d3
Weuller Marcos da Silva Santos <weuller@redecfa.org.br>
2017-09-18 16:14:54 -0300

    Criando Spinner

--- PeladaEsporteClube/app/src/main/java/com/example/weuller/peladaesporteclube/ScheduleFootballActivity.java
+++ PeladaEsporteClube/app/src/main/java/com/example/weuller/peladaesporteclube/ScheduleFootballActivity.java
@@ -4,8 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import java.util.Calendar;
 
@@ -13,6 +15,10 @@ public class ScheduleFootballActivity extends AppCompatActivity {
 
     private EditText edtDate, edtTime;
     private String selectedDate, selectedTime;
+    private Spinner spnType;
+    private Button btnFindSuggested;
+
+    ArrayAdapter<String> adpType;
 
     public String getSelectedDate() {
         return selectedDate;
@@ -37,12 +43,20 @@ public class ScheduleFootballActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_football);
 
+        spnType = (Spinner) findViewById(R.id.schedule_football_spnType);
         edtDate = (EditText) findViewById(R.id.schedule_football_edtDate);
         edtTime = (EditText) findViewById(R.id.schedule_football_edtTime);
+        btnFindSuggested = (Button) findViewById(R.id.schedule_football_btnFindSuggested);
 
         edtDate.setText(getCurrentDate());
         edtTime.setText(getCurrentTime());
 
+        //configurando adapter de Tipos
+        adpType = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        adpType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnType.setAdapter(adpType);
+
+        populaAdapter();
 
         edtDate.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -69,6 +83,14 @@ public class ScheduleFootballActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        btnFindSuggested.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
     }
 
     private String getCurrentDate(){
@@ -88,5 +110,12 @@ public class ScheduleFootballActivity extends AppCompatActivity {
         int minute = c.get(Calendar.MINUTE);
 
         return Integer.toString(hour) + ":" + Integer.toString(minute);
+    }
+
+    private void populaAdapter() {
+
+        adpType.add("Campo");
+        adpType.add("Quadra");
+        adpType.add("Society");
     }
 }
