f164c0189decc6e9aab17eb997076c77356578d5
Weuller Marcos <weullermarcos@MacBook-Pro-de-Weuller.local>
2017-09-19 23:25:02 -0300

    adicionando classe de partida de futebol

---
+++ PeladaEsporteClube/app/src/main/java/com/example/weuller/peladaesporteclube/Models/FootballMatch.java
@@ -0,0 +1,63 @@
+package com.example.weuller.peladaesporteclube.Models;
+
+/**
+ * Created by weullermarcos on 19/09/17.
+ */
+
+public class FootballMatch {
+
+    private String date;
+    private String footballFieldId;
+    private double footballFieldLatitude;
+    private double footballFieldLongitude;
+    private String footballFieldName;
+    private String hour;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getFootballFieldId() {
+        return footballFieldId;
+    }
+
+    public void setFootballFieldId(String footballFieldId) {
+        this.footballFieldId = footballFieldId;
+    }
+
+    public double getFootballFieldLatitude() {
+        return footballFieldLatitude;
+    }
+
+    public void setFootballFieldLatitude(double footballFieldLatitude) {
+        this.footballFieldLatitude = footballFieldLatitude;
+    }
+
+    public double getFootballFieldLongitude() {
+        return footballFieldLongitude;
+    }
+
+    public void setFootballFieldLongitude(double footballFieldLongitude) {
+        this.footballFieldLongitude = footballFieldLongitude;
+    }
+
+    public String getFootballFieldName() {
+        return footballFieldName;
+    }
+
+    public void setFootballFieldName(String footballFieldName) {
+        this.footballFieldName = footballFieldName;
+    }
+
+    public String getHour() {
+        return hour;
+    }
+
+    public void setHour(String hour) {
+        this.hour = hour;
+    }
+}
